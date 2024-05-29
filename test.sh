028c90e14f8f432a8dbd4027c012f280350e68d2
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-05-30 01:18:09 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-curl --location 'http://localhost:8000/v2/models/nvidia-streaming-sse-2/versions/1/generate_stream'           --header 'Accept: text/event-stream'           --data '{
+curl --location 'http://localhost:8000/v2/models/nvidia-triton-llm-streaming/versions/1/generate_stream'           --header 'Accept: text/event-stream'           --data '{
   "parameters" :
     {
       "TEXT" : "How to make a omellete"
